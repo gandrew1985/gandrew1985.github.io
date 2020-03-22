f5819c3d908e3ddd82003b42c92900ad040c7d0c
gandrew1985 <gandrew@op.pl>
2020-03-22 12:25:40 +0000

    final6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
   const apiRoot = 'https://gentle-escarpment-14259.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://gentle-escarpment-14259.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
